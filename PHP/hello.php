ad8c4b2170504f831372e72830ea98665696b338
prashanthbiradar345 <37619647+prashanthbiradar345@users.noreply.github.com>
2024-03-20 20:41:04 +0530

    Add files via upload
    added images and files

---
+++ PHP/hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
+
+
+
+
+https://mail.google.com/mail/?view=cm&fs=1&to=sajjanbiradar7021@gmail.com&su=SUBJECT&body=BODY
